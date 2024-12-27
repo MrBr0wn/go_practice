c6ffe802c4abb1224d35231efb0c5127d47d7ead
Mr. Brown <gitcherrypick.work@gmail.com>
2024-12-28 00:05:24 +0500

    practice: structs

---
+++ ya_practicum/structs/structs.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"time"
+)
+
+// Для сериализации используется функция json.Marshal() пакета json. Дана структура:
+// type Person struct {
+//		Name        string
+//		Email       string
+//		DateOfBirth time.Time
+// }
+// Напишите код, который будет сериализовывать структуру в json-строку следующего вида:
+// {
+//		"Имя": "Aлекс",
+//		"Почта": "alex@yandex.ru"
+// }
+
+type Person struct {
+	Name        string    `json:"Name"`
+	Email       string    `json:"Email"`
+	DateOfBirth time.Time `json:"-"`
+}
+
+func main() {
+	fmt.Println("Exercise 2: ")
+	man := Person{
+		Name:        "Alex",
+		Email:       "alex@example.com",
+		DateOfBirth: time.Now(),
+	}
+	jsMan, err := json.Marshal(man)
+	if err != nil {
+		log.Fatalln("unable marshal to json")
+	}
+	fmt.Printf("Man %v", string(jsMan))
+
+	fmt.Println("Exercise 3:")
+
+	type Response struct {
+		Header struct {
+			Code    int    `json:"code"`
+			Message string `json:"message,omitempty"`
+		} `json:"header"`
+		Data []struct {
+			Type       string `json:"type"`
+			Id         int    `json:"id"`
+			Attributes struct {
+				Email      string `json:"email"`
+				ArticleIds []int  `json:"article_ids"`
+			}
+		} `json:"data,omitempty"`
+	}
+
+	input := `
+		{
+				"header": {
+						"code": 0,
+						"message": "aaa"
+				},
+				"data": [{
+						"type": "user",
+						"id": 100,
+						"attributes": {
+								"email": "bob@yandex.ru",
+								"article_ids": [10, 11, 12]
+						}
+				}]
+		}`
+	var res Response // eq res := Response{}
+	err1 := json.Unmarshal([]byte(input), &res)
+
+	if err1 != nil {
+		fmt.Println("Unmarshaling error", err)
+	}
+	fmt.Printf("Response: %v\n", res)
+}
