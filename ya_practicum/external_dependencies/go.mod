5e7b64ae5304861f116a8d85bd1b96e450ac5f57
Mr. Brown <gitcherrypick.work@gmail.com>
2025-01-17 01:27:05 +0500

    ya_pract: external dependencies

---
+++ ya_practicum/external_dependencies/go.mod
@@ -0,0 +1,5 @@
+module main
+
+go 1.23.3
+
+require github.com/MrBr0wn/mymath v0.0.2
---
+++ ya_practicum/external_dependencies/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/MrBr0wn/mymath"
+)
+
+func main() {
+	if sum := mymath.Add(1, 2); sum != 3 {
+		panic(fmt.Sprintf("sum expected to be 3; got %d", sum))
+	}
+
+	fmt.Println("Well done!")
+}
